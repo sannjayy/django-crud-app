774c36dadd55265baa4ededdc5f58fbfa1aec238
sannjayy <znasofficial@gmail.com>
2021-01-29 16:38:56 +0530

    Added Readme File

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import UserModel
 # Register your models here.
 
+admin.site.site_header = "Django CRUD App"
+admin.site.site_title = "Django CRUD App Admin Portal"
+admin.site.index_title = "Welcome to Django CRUD App by Sanjay"
+
+
 @admin.register(UserModel)
 class UserDataAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'email', 'gender', 'date_created']
